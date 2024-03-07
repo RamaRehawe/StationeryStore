ac7178b1269d84b8b3673c67570e02018cd93976
RamaRehawe <ramarehawe51@gmail.com>
2024-03-07 18:56:03 +0300

    build after models

--- obj/Debug/net8.0/StationeryStore.AssemblyInfo.cs
+++ obj/Debug/net8.0/StationeryStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StationeryStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07b5e5e94436ca1be033ad8b18538577f27e2e9c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf9ac965b1d1a493c71a0a5d71d64d99acd67199")]
 [assembly: System.Reflection.AssemblyProductAttribute("StationeryStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StationeryStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
