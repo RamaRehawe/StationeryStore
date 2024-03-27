57d382083bd3397c6b1a2106b4aed049bfe693f0
Oula Nassri <137612493+Oula97@users.noreply.github.com>
2024-03-27 14:30:10 +0100

    Create SignupController.cs

---
+++ Views/Signup/SignupController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using StationeryStore.Models;
+using System.Threading.Tasks;
+
+namespace StationeryStore.Views.Singup
+{
+    public class SignupController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(); // Assuming you have a corresponding View file named Signup.cshtml
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(string username, string email, string password)
+        {
+            // Implement your logic to create a new user with the provided username, email, and password
+            // You may also want to send a confirmation email to the user
+
+            // Example:
+            // var user = new User { Username = username, Email = email, Password = password };
+            // await _userRepository.AddUserAsync(user);
+            // await _emailService.SendConfirmationEmailAsync(email, confirmationCode);
+
+            return RedirectToAction("Confirmation"); // Redirect to a confirmation page
+        }
+
+        public IActionResult Confirmation()
+        {
+            return View(); // Assuming you have a corresponding View file named Confirmation.cshtml
+        }
+    }
+}
