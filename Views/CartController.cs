2e41ff93241a82d85d9f7f2e9de4fd791da15217
Oula Nassri <137612493+Oula97@users.noreply.github.com>
2024-03-30 12:51:27 +0100

    Add files via upload

---
+++ Views/CartController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StationeryStore.Views.Cart
+{
+    public class CartController : Controller
+    {
+        [HttpGet]
+        [Route("front_cart")]
+        public IActionResult Index()
+        {
+            return View("Cart");
+        }
+    }
+}
