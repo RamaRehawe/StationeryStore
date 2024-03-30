bd1c16bcb0279ff76ee30066f93b8a9f5b999a63
Oula Nassri <137612493+Oula97@users.noreply.github.com>
2024-03-30 13:58:15 +0100

    Add files via upload

---
+++ Views/ItemController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StationeryStore.Views
+{
+    public class ItemController : Controller
+    {
+        [HttpGet]
+        [Route("front_item")]
+        public IActionResult Index()
+        {
+            return View("Item");
+        }
+    }
+}
