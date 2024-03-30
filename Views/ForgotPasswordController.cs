4540390e16ecc8320f38b5879c64994fe8641124
Oula Nassri <137612493+Oula97@users.noreply.github.com>
2024-03-30 13:29:09 +0100

    Add files via upload

---
+++ Views/ForgotPasswordController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StationeryStore.Views.ForgotPassword
+{
+    public class ForgotPasswordController : Controller
+    {
+        [HttpGet]
+        [Route("front_forgot_password")]
+        public IActionResult Index()
+        {
+            return View("ForgotPassword");
+        }
+    }
+}
