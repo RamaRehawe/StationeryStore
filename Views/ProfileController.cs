19c67143eb2224f3e55337307f97afdf11ecc736
Oula Nassri <137612493+Oula97@users.noreply.github.com>
2024-03-30 14:51:12 +0100

    Add files via upload

---
+++ Views/ProfileController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StationeryStore.Views.Profile
+{
+    public class ProfileController : Controller
+    {
+        [HttpGet]
+        [Route("front_proile")]
+        public IActionResult Index()
+        {
+            return View("Profile");
+        }
+    }
+}
