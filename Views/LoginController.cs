92cc10fc2f038b4fa29e8953bd6661c6e7092fc3
Oula Nassri <137612493+Oula97@users.noreply.github.com>
2024-03-30 12:16:13 +0100

    Add files via upload

---
+++ Views/LoginController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StationeryStore.Views.Login
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        [Route("front_login")]
+        public IActionResult Index()
+        {
+            return View("Login") ;
+        }
+    }
+}
