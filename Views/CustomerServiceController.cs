b1044dd6cbfe0a7a7a7da54181476f2aa4b885fa
Oula Nassri <137612493+Oula97@users.noreply.github.com>
2024-03-30 11:46:55 +0100

    Add files via upload

---
+++ Views/CustomerServiceController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StationeryStore.Views.CustomerService
+{
+    public class CustomerServiceController : Controller
+    {
+        [HttpGet]
+        [Route("front_customer_service")]
+        public IActionResult Index()
+        {
+            return View("CustomerService") ;
+        }
+    }
+}
---
+++ Views/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StationeryStore.Views.Home
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        [Route("front_home")]
+        public IActionResult Index()
+        {
+            return View("Home");
+        }
+    }
+}
