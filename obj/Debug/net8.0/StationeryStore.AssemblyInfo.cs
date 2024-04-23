b397bbf72fce8a63210eaec709c83e45d1bfe71d
RamaRehawe <ramarehawe51@gmail.com>
2024-04-24 01:33:26 +0300

    ..

--- obj/Debug/net8.0/StationeryStore.AssemblyInfo.cs
+++ obj/Debug/net8.0/StationeryStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StationeryStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4459779eb0120182b2eded530c66e17836ac6e15")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+011b923677fe6d761eec3969fa6cd394ddee8f4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("StationeryStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StationeryStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
