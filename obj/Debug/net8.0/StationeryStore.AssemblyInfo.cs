951d351bfd9ce4f5f005787b6d8f687b245dff12
Oula97 <137612493+Oula97@users.noreply.github.com>
2024-04-30 01:36:22 +0300

    edit

--- obj/Debug/net8.0/StationeryStore.AssemblyInfo.cs
+++ obj/Debug/net8.0/StationeryStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StationeryStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14d2e46fe98652b6c34fc02e9ace54e751cd857d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a826349afed7952c014758734ba3038441370d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("StationeryStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StationeryStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
